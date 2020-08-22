dfeb88740e10b497d52d09f147acdcea6f3811f2
Camila_Oyarzún <caoyarz@alumnos.ubiobio.cl>
2020-08-22 16:06:46 -0400

    proyecto base

---
+++ src/views/Burguers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="Burguers">
+    <h1>Completa aquí el desafío</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Burguers",
+};
+</script>
+
+<style>
+</style>
