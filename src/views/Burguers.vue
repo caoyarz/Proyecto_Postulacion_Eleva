7a09ddf55b119a90abdf8d2cadd105758e32205a
Camila_Oyarzún <caoyarz@alumnos.ubiobio.cl>
2020-08-23 12:51:56 -0400

    incluye codigo para modificacion de datos - no terminado

--- src/views/Burguers.vue
+++ src/views/Burguers.vue
@@ -5,7 +5,7 @@
     Hamburguesas
      
     </div>
-   
+   <!-----------------------------TABLA PARA MOSTRAR EL CRUD------------------- -->
     <table >
      <tr>
        <th>Nombre</th>
@@ -21,6 +21,7 @@
         <button class="boton_elimina"  @click="confirmarEliminacion(hamburguesa._id)"></button></td>
      </tr>
     </table>    
+
     <!--  POPUP PARA MOSTRAR LOS DETALLES-------------------------------------->
     <div class="overlay"  v-if="detalles">
       <div  class="ventana_detalles">       
@@ -36,19 +37,21 @@
 
     
     <!--POPUP PARA MODIFICAR UNA HAMBURGUESA------------------------------------->
-      <!--
     <div class="overlay"  v-if="modificar">
       <div  class="ventana_detalles">       
-        <strong>Nombre:</strong> {{hamburguesaSeleccionada[0].nombre}}<br>
-        <strong>Calorías:</strong> {{hamburguesaSeleccionada[0].calorias}}<br>
+        <strong>Nombre: </strong> 
+        <input type="text" v-bind:placeholder="hamburguesaSeleccionada[0].nombre" v-model="nombreM"><br>
+        <strong>Calorías:</strong> 
+        <input type="number" v-bind:placeholder="hamburguesaSeleccionada[0].calorias" v-model="caloriasM"><br>
         <strong>Ingredientes:</strong>
         <ul class="ingrediente">
-        <li  v-for="ingrediente in hamburguesaSeleccionada[0].ingredientes" :key="ingrediente">{{ingrediente}}</li>
+        <li  v-for="ingrediente in hamburguesaSeleccionada[0].ingredientes" :key="ingrediente"><input type="text" v-bind:placeholder="ingrediente" v-model="ingredientesM"></li>
         </ul>       
+        <button @click="actualizarHamburguesa(hamburguesaSeleccionada._id,)" class="boton_actualizar">Actualizar</button>
+        <br>
         <button @click="modificar=false" class="boton_cerrar_info">Cerrar</button>
       </div>
-    </div>-->
-    <!--POPUP PARA ELIMINAR UNA HAMBURGUESA------------------------------------->
+    </div>
 
 
 
@@ -63,15 +66,19 @@ export default {
     return {
       hamburguesas: null,
       detalles: false,
-      eliminar: false,
       modificar: false,
       hamburguesaSeleccionada: {},
+      //variables que guardan los datos de la modificaion
+      nombreM: "",
+      caloriasM: "",
+      ingredientesM: [],
     };
   },
   mounted() {
     this.getHamburguesas();
   },
   methods: {
+    //CONEXION A LA API PARA TRAER LOS DATOS
     getHamburguesas() {
       axios
         .get("https://prueba-hamburguesas.herokuapp.com/burguer")
@@ -87,6 +94,7 @@ export default {
       );
       this.detalles = true;
     },
+    //FUNCION QUE CONFIRMA LA ELIMINACION MEDIANTE UNA VENTANA DE ALERTA
     confirmarEliminacion: function(id) {
       this.hamburguesaSeleccionada = this.hamburguesas.filter(
         (burger) => burger._id === id
@@ -97,10 +105,31 @@ export default {
         this.eliminarHamburguesa(id);
       }
     },
-
+    //FUNCION QUE ELIMINA
     eliminarHamburguesa: function(id) {
       axios
         .delete("https://prueba-hamburguesas.herokuapp.com/burguer/" + id)
+        .then((respuesta) => {
+          this.getHamburguesas();
+        })
+        .catch((e) => console.log(e));
+    },
+    //SELECCIONA UNA HAMBURGUESA Y LLAMA A LA VENTANA DONDE SE MUESTRAN LOS DATOS PARA ACTUALIZAR
+    actualizaHamburguesas: function(id) {
+      this.hamburguesaSeleccionada = this.hamburguesas.filter(
+        (burger) => burger._id === id
+      );
+      this.modificar = true;
+    },
+
+    // PUT PARA MODIFICAR LA INFORMACION -- NO LOGRADO
+    actualizarHamburguesa: function(id) {
+      axios
+        .put("https://prueba-hamburguesas.herokuapp.com/burguer/" + id, {
+          nombre: this.nombreM,
+          calorias: this.caloriasM,
+          ingredientes: this.ingredientesM,
+        })
         .then((respuesta) => {
           this.getHamburguesas();
         })
@@ -253,5 +282,25 @@ table tr:nth-child(odd) {
 .ventana_agregar label {
   font-weight: bold;
   padding: 5px;
+}
+input {
+  border: 0;
+}
+.boton_actualizar {
+  background: #3551ad;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+  border: 0px;
+  color: white;
+  font-weight: bold;
+  font-size: 12pt;
+  cursor: pointer;
+  display: flex;
+  margin: auto;
+  justify-content: center;
+}
+.boton_actualizar:hover {
+  background: #273c80;
 }
 </style>
