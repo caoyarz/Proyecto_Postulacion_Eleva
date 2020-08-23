89377f542bc00759ef7aeefb9f1e1002e47b9419
Camila_Oyarzún <caoyarz@alumnos.ubiobio.cl>
2020-08-23 01:17:13 -0400

    agrega vista detalle de hamburguesas

--- src/views/Burguers.vue
+++ src/views/Burguers.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
     <h1>Hamburguesas</h1>
+    <button class="boton_nueva">Nueva Hamburguesa</button>
     <table >
      <tr>
        <th>Nombre</th>
        <th>Calorías</th>
        <th>Ver detalle</th>
+       <th>Acciones</th>
      </tr>
      <tr v-for="hamburguesa in hamburguesas" :key="hamburguesa.id">
        <td>{{hamburguesa.nombre}}</td>
-       <td>{{hamburguesa.calorias}}</td>
-       <td> <img alt="Icono hamburguesa" src="../assets/hamburguesa.png"></td>
-
+       <td>{{hamburguesa.calorias}}</td>       
+       <td> <button @click="detalleHamburguesas(hamburguesa._id)"  class="boton_detalles" ></button></td>
+       <!--<td> <button @click.prevent="ventana=true" class="boton_detalles" :key="hamburguesa.id" >Modificar</button></td>
+       <td> <button @click.prevent="ventana=true" class="boton_detalles" :key="hamburguesa.id" >Eliminar</button></td>-->
      </tr>
-    </table>
-    
+    </table>    
+
+    <div class="overlay" v-bind="ventana" v-if="ventana">
+      <div  class="ventana_detalles">       
+        <strong>Nombre:</strong> {{hamburguesaSeleccionada[0].nombre}}<br>
+        <strong>Calorías:</strong>{{hamburguesaSeleccionada[0].calorias}}<br>
+        <strong>Ingredientes:</strong>
+        <ul class="ingrediente">
+        <li  v-for="ingrediente in hamburguesaSeleccionada[0].ingredientes" :key="ingrediente">{{ingrediente}}</li>
+        </ul>
+       
+        <button @click.prevent="ventana=false" class="boton_cerrar_info">Cerrar</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,6 +39,8 @@ export default {
   data() {
     return {
       hamburguesas: null,
+      ventana: false,
+      hamburguesaSeleccionada: null,
     };
   },
   mounted() {
@@ -33,20 +50,30 @@ export default {
     getHamburguesas() {
       axios
         .get("https://prueba-hamburguesas.herokuapp.com/burguer")
-        .then((response) => {
-          this.hamburguesas = response.data;
+        .then((respuesta) => {
+          this.hamburguesas = respuesta.data;
         })
         .catch((e) => console.log(e));
+    },
+
+    detalleHamburguesas: function(id) {
+      this.hamburguesaSeleccionada = this.hamburguesas.filter(
+        (burger) => burger._id === id
+      );
+
+      this.ventana = true;
     },
   },
 };
 </script>
 
 <style>
+template {
+  font-family: Arial, Helvetica, sans-serif;
+}
 table {
   border-collapse: collapse;
   font-size: 12pt;
-  font-family: Arial, Helvetica, sans-serif;
   width: 80%;
   text-align: center;
   margin: auto;
@@ -63,5 +90,76 @@ td {
 
 table tr:nth-child(odd) {
   background: rgba(250, 245, 245, 0.945);
+}
+.boton_detalles {
+  background-image: url("../assets/hamburguesa.png");
+  background-size: cover;
+  border: none;
+  padding: 20px;
+  cursor: pointer;
+  transition: all 0.5s;
+}
+.boton_detalles:hover {
+  width: 45px;
+  height: 45px;
+}
+
+.boton_nueva {
+  background: #42b983;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+  border: 0px;
+  color: white;
+  font-weight: bold;
+  font-size: 12pt;
+  cursor: pointer;
+}
+.boton_nueva:hover {
+  background: #30855f;
+}
+.boton_cerrar_info {
+  background: #42b983;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+  border: 0px;
+  color: white;
+  font-weight: bold;
+  font-size: 12pt;
+  cursor: pointer;
+  display: flex;
+  margin: auto;
+  justify-content: center;
+}
+.boton_cerrar_info:hover {
+  background: #30855f;
+}
+.overlay {
+  background: rgb(0, 0, 0, 0.3);
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.ventana_detalles {
+  background: #f8f8f8;
+  box-shadow: 0px 0px 5px 5px rgb(0, 0, 0, 0.3);
+  border-radius: 10px;
+  padding: 30px;
+  width: 350px;
+  height: auto;
+  text-align: left;
+}
+
+.ingrediente {
+  list-style: none;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: left;
 }
 </style>
